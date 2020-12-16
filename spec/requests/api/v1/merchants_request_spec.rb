9064d592775d92db579ce14f3eb439ed65953584
Eduardo Parra <helloeduardo@icloud.com>
2020-12-15 18:53:47 -0700

    Add merchant show

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -18,4 +18,20 @@ RSpec.describe 'Merchants API', type: :request do
       expect(merchant[:attributes][:name]).to be_a(String)
     end
   end
+
+  it 'shows a merchant' do
+    id = create(:merchant).id
+
+    get "/api/v1/merchants/#{id}"
+
+    expect(response).to be_successful
+
+    merchant = JSON.parse(response.body, symbolize_names: true)[:data]
+
+    expect(merchant).to have_key(:id)
+    expect(merchant[:id]).to be_an(String)
+
+    expect(merchant[:attributes]).to have_key(:name)
+    expect(merchant[:attributes][:name]).to be_a(String)
+  end
 end
