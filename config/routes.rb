0527eb766d9d3bbda94a4e4c69f5cfc508b561fd
Eduardo Parra <helloeduardo@icloud.com>
2020-12-16 16:12:48 -0700

    Add merchant find route

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       namespace :merchants do
+        get '/find', to: 'search#show'
         get '/:id/items', to: 'items#index'
       end
       resources :merchants
