b0d3959eb8aba6dd3e1031baa91e343c6f10651b
Eduardo Parra <helloeduardo@icloud.com>
2020-12-15 21:30:23 -0700

    Add merchant items route

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :merchants
+      resources :merchants do
+        resources :items, only: [:index], module: :merchants
+      end
       resources :items
     end
   end
