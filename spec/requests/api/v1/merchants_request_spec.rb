507a94807a1b7baac4d8244506ab69667507b527
Eduardo Parra <helloeduardo@icloud.com>
2020-12-15 18:48:52 -0700

    Add Merchant Index API spec

---
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -0,0 +1,21 @@
+RSpec.describe 'Merchants API', type: :request do
+  it 'gets merchants' do
+    create_list(:merchant, 3)
+
+    get '/api/v1/merchants'
+
+    expect(response).to be_successful
+
+    merchants = JSON.parse(response.body, symbolize_names: true)[:data]
+
+    expect(merchants.count).to eq(3)
+
+    merchants.each do |merchant|
+      expect(merchant).to have_key(:id)
+      expect(merchant[:id]).to be_an(String)
+
+      expect(merchant[:attributes]).to have_key(:name)
+      expect(merchant[:attributes][:name]).to be_a(String)
+    end
+  end
+end
