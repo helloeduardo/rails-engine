fe7a3b31a7720dfb4038b2ac3d563c0f06514add
Eduardo Parra <helloeduardo@icloud.com>
2020-12-16 16:12:56 -0700

    Add search method

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -3,4 +3,11 @@ class Merchant < ApplicationRecord
   has_many :invoices
 
   validates :name, presence: true
+
+  def self.search(params)
+    attribute = arel_table[params.keys.first]
+    value = "%#{params.values.first}%"
+
+    find_by(attribute.matches(value))
+  end
 end
