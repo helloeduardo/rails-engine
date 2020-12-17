791e5443902ba415a5893383486dd14e4dc4bc52
Eduardo Parra <helloeduardo@icloud.com>
2020-12-16 18:42:26 -0700

    Add find endpoint spec

--- spec/requests/api/v1/merchants/search_request_spec.rb
+++ spec/requests/api/v1/merchants/search_request_spec.rb
@@ -1,58 +1,80 @@
 RSpec.describe 'Search Merchants API', type: :request do
-  it 'can find a merchant by exact attribute match' do
-    create_list(:merchant, 3)
+  describe '/find? endpoint' do
+    it 'can find a merchant by exact attribute match' do
+      create_list(:merchant, 3)
 
-    merchant = Merchant.last
+      merchant = Merchant.last
 
-    get "/api/v1/merchants/find?name=#{merchant.name}"
+      get "/api/v1/merchants/find?name=#{merchant.name}"
 
-    expect(response).to be_successful
+      expect(response).to be_successful
 
-    merchant_result = JSON.parse(response.body, symbolize_names: true)[:data]
+      merchant_result = JSON.parse(response.body, symbolize_names: true)[:data]
 
-    expect(merchant_result).to have_key(:id)
-    expect(merchant_result[:id]).to eq(merchant.id.to_s)
+      expect(merchant_result).to have_key(:id)
+      expect(merchant_result[:id]).to eq(merchant.id.to_s)
 
-    expect(merchant_result[:attributes]).to have_key(:name)
-    expect(merchant_result[:attributes][:name]).to eq(merchant.name)
-  end
+      expect(merchant_result[:attributes]).to have_key(:name)
+      expect(merchant_result[:attributes][:name]).to eq(merchant.name)
+    end
 
-  it 'can find a merchant by case-insensitive partial attribute match' do
-    create_list(:merchant, 3)
+    it 'can find a merchant by case-insensitive partial attribute match' do
+      create_list(:merchant, 3)
 
-    merchant = create(:merchant, name: "Wal-Mart")
+      merchant = create(:merchant, name: "Wal-Mart")
 
-    get "/api/v1/merchants/find?name=ARt"
+      get "/api/v1/merchants/find?name=ARt"
 
-    expect(response).to be_successful
+      expect(response).to be_successful
 
-    merchant_result = JSON.parse(response.body, symbolize_names: true)[:data]
+      merchant_result = JSON.parse(response.body, symbolize_names: true)[:data]
 
-    expect(merchant_result).to have_key(:id)
-    expect(merchant_result[:id]).to eq(merchant.id.to_s)
+      expect(merchant_result).to have_key(:id)
+      expect(merchant_result[:id]).to eq(merchant.id.to_s)
 
-    expect(merchant_result[:attributes]).to have_key(:name)
-    expect(merchant_result[:attributes][:name]).to eq(merchant.name)
-  end
+      expect(merchant_result[:attributes]).to have_key(:name)
+      expect(merchant_result[:attributes][:name]).to eq(merchant.name)
+    end
 
-  xit 'can find all merchants by attribute' do
-    create_list(:merchant, 3)
+    it 'can find a merchant by date attribute match' do
+      create_list(:merchant, 3)
 
-    get '/api/v1/merchants'
+      merchant = create(:merchant, created_at: 3.days.ago)
 
-    expect(response).to be_successful
+      get "/api/v1/merchants/find?created_at=#{merchant.created_at}"
 
-    merchants = JSON.parse(response.body, symbolize_names: true)[:data]
+      expect(response).to be_successful
 
-    expect(merchants.count).to eq(3)
+      merchant_result = JSON.parse(response.body, symbolize_names: true)[:data]
 
-    merchants.each do |merchant|
-      expect(merchant).to have_key(:id)
-      expect(merchant[:id]).to be_an(String)
+      expect(merchant_result).to have_key(:id)
+      expect(merchant_result[:id]).to eq(merchant.id.to_s)
 
-      expect(merchant[:attributes]).to have_key(:name)
-      expect(merchant[:attributes][:name]).to be_a(String)
+      expect(merchant_result[:attributes]).to have_key(:name)
+      expect(merchant_result[:attributes][:name]).to eq(merchant.name)
     end
   end
 
+  describe '/find_all? endpoint' do
+    it 'can find all merchants by attribute' do
+      create_list(:merchant, 3)
+
+      get '/api/v1/merchants'
+
+      expect(response).to be_successful
+
+      merchants = JSON.parse(response.body, symbolize_names: true)[:data]
+
+      expect(merchants.count).to eq(3)
+
+      merchants.each do |merchant|
+        expect(merchant).to have_key(:id)
+        expect(merchant[:id]).to be_an(String)
+
+        expect(merchant[:attributes]).to have_key(:name)
+        expect(merchant[:attributes][:name]).to be_a(String)
+      end
+    end
+
+  end
 end
