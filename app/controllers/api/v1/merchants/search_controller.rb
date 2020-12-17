ea891847091ad0736856acc9db6450374244f041
Eduardo Parra <helloeduardo@icloud.com>
2020-12-16 18:42:58 -0700

    Add private attribute and value params

--- app/controllers/api/v1/merchants/search_controller.rb
+++ app/controllers/api/v1/merchants/search_controller.rb
@@ -1,11 +1,19 @@
 class Api::V1::Merchants::SearchController < ApplicationController
   def show
-    merchant = Merchant.search(merchant_params)
+    merchant = Merchant.single_search(attribute, value)
     render json: MerchantSerializer.new(merchant)
   end
 
   private
     def merchant_params
-      params.permit(:name)
+      params.permit(:name, :created_at, :updated_at)
+    end
+
+    def attribute
+      merchant_params.keys.first
+    end
+
+    def value
+      merchant_params[attribute]
     end
 end
