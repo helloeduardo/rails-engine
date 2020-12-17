9896cdba4a6591a48efd97c68e537cb6827d23f8
Eduardo Parra <helloeduardo@icloud.com>
2020-12-16 21:11:10 -0700

    Add simple cov filters

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,6 +15,7 @@
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 require 'simplecov'
 SimpleCov.start 'rails'
+SimpleCov.add_filter ["app/channels", "app/helpers", "app/mailers", "app/jobs"]
 
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
