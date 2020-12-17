3a2260c05f19c6d9c408600f070927a7b66c8e95
Eduardo Parra <helloeduardo@icloud.com>
2020-12-16 21:11:57 -0700

    Move search methods to application record

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -3,20 +3,4 @@ class Merchant < ApplicationRecord
   has_many :invoices
 
   validates :name, presence: true
-
-  def self.single_search(attribute, value)
-    if attribute == 'created_at' || attribute == 'updated_at'
-      find_by("DATE(#{attribute}) = '%#{value.to_date}%'")
-    else
-      find_by("LOWER(#{attribute}) LIKE ?", "%#{value.downcase}%")
-    end
-  end
-
-  def self.multi_search(attribute, value)
-    if attribute == 'created_at' || attribute == 'updated_at'
-      where("DATE(#{attribute}) = '%#{value.to_date}%'")
-    else
-      where("LOWER(#{attribute}) LIKE ?", "%#{value.downcase}%")
-    end
-  end
 end
