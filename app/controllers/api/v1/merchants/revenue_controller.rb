a941a488030d8c4d905238912b06e0d17415a34a
Eduardo Parra <helloeduardo@icloud.com>
2020-12-17 20:47:33 -0700

    Add index method

--- app/controllers/api/v1/merchants/revenue_controller.rb
+++ app/controllers/api/v1/merchants/revenue_controller.rb
@@ -1,4 +1,9 @@
 class Api::V1::Merchants::RevenueController < ApplicationController
+  def index
+    revenue = Merchant.total_revenue_between_dates(params[:start], params[:end])
+    render json: RevenueSerializer.revenue(revenue)
+  end
+
   def show
     revenue = Merchant.find(params[:id]).revenue
     render json: RevenueSerializer.revenue(revenue)
