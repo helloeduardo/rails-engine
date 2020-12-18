70d89fbc24cc28eac4e4c5a0dbc3acc703cb48b7
Eduardo Parra <helloeduardo@icloud.com>
2020-12-17 17:39:19 -0700

    Add with revenue trait to merchant factory

--- spec/factories/merchants.rb
+++ spec/factories/merchants.rb
@@ -11,5 +11,19 @@ FactoryBot.define do
         create_list(:item, evaluator.item_count, merchant: merchant)
       end
     end
+
+    trait :with_revenue do
+      after :create do |merchant|
+        3.times do
+          invoice = create(:invoice, merchant: merchant)
+          item = create(:item, merchant: merchant)
+          item_2 = create(:item, merchant: merchant)
+
+          create(:invoice_item, item: item, invoice: invoice, unit_price: item.unit_price)
+          create(:invoice_item, item: item_2, invoice: invoice, unit_price: item_2.unit_price)
+          create(:transaction, invoice: invoice)
+        end
+      end
+    end
   end
 end
