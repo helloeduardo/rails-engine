9bf3e132cceca6c715a552e1d7428a2e5d252d12
Eduardo Parra <helloeduardo@icloud.com>
2020-12-17 17:42:22 -0700

    Add most_revenue route

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
       namespace :merchants do
         get '/find', to: 'search#show'
         get '/find_all', to: 'search#index'
+        get '/most_revenue', to: 'search#most_revenue'
         get '/:id/items', to: 'items#index'
       end
       resources :merchants
