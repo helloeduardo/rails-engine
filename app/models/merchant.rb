61d7025c7bf4d7dbd1a1fb651634183129523f2b
Eduardo Parra <helloeduardo@icloud.com>
2020-12-17 17:42:40 -0700

    Add most revenue and revenue methods

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -3,4 +3,22 @@ class Merchant < ApplicationRecord
   has_many :invoices
 
   validates :name, presence: true
+
+  def self.most_revenue(quantity)
+    joins(invoices: [:invoice_items, :transactions])
+      .select('merchants.*, SUM(invoice_items.unit_price * invoice_items.quantity) AS revenue')
+      .merge(Transaction.successful)
+      .merge(Invoice.shipped)
+      .order('revenue DESC')
+      .group(:id)
+      .limit(quantity)
+  end
+
+  def revenue
+    invoices.joins(:invoice_items, :transactions)
+      .merge(Transaction.successful)
+      .merge(Invoice.shipped)
+      .sum('invoice_items.unit_price * invoice_items.quantity')
+      .round(2)
+  end
 end
